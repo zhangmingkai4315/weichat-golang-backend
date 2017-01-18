edf64c52e92c6263aa12baaa88f7e7ef334b7e01
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-01-18 22:39:03 +0800

    add the bind vlist work

--- worker/scrapy-isc.go
+++ worker/scrapy-isc.go
@@ -4,10 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"log"
+	//"log"
 	"strconv"
 	"strings"
 	"sync"
+	"sync/atomic"
+	"log"
+	"encoding/json"
+	"os"
 )
 
 const (
@@ -25,7 +29,7 @@ const (
 	QUERY_STOPPED
 	QUERY_ERROR
 )
-
+const MAXCONCURRENT = 5
 var (
 	ErrUnequalResults     = errors.New("Unequal results and urls return")
 	ErrOutOfSelectedRange = errors.New("Selected range out of 3")
@@ -33,15 +37,13 @@ var (
 
 type BindValunerabilityDetail struct {
 	CVE             string  `json:cve`
-	DocumentVersion float32 `json:document_version`
+	DocumentVersion string `json:document_version`
 	PostingDate     string  `json:posting_date`
 	VersionAffected string  `json:"version_affected"`
 	Severity        string  `json:"severity"`
 	Expoitable      string  `json:"expoitable"`
 	Description     string  `json:"description"`
 	Impact          string  `json:"impact"`
-	ActiveExpoits   string  `json:"active_expoits"`
-	Solution        string  `json:"solution"`
 }
 type BindValunerability struct {
 	ID               int                      `json:id`
@@ -51,6 +53,43 @@ type BindValunerability struct {
 	Status           status                   `json:status`
 	Detail           BindValunerabilityDetail `json:detail`
 }
+func NewBindValunerabiltiy() *BindValunerability {
+	return &BindValunerability{
+		Status: QUERY_NOTSTART,
+		Detail: BindValunerabilityDetail{},
+	}
+}
+func NewBindValunerabilityDetail(cve string)*BindValunerabilityDetail{
+	return &BindValunerabilityDetail{CVE:cve}
+}
+
+func splitAndLast(s string)string{
+	temp:=strings.Split(s,":")
+	if len(temp)>1{
+		return strings.TrimSpace(temp[1])
+	}else{
+		return "None"
+	}
+}
+func(self *BindValunerabilityDetail)GetDetailInfo(cve,url string)error{
+	self.CVE=cve
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return err
+	}
+	self.DocumentVersion=splitAndLast(doc.Find(".txt .field-field-document-version").Last().Text())
+	self.PostingDate=splitAndLast(doc.Find(".txt .field-field-date").Last().Text())
+	self.Impact=splitAndLast(doc.Find(".txt .field-field-project").Last().Text())
+	self.VersionAffected=splitAndLast(doc.Find(".txt .field-field-versions-affected").Last().Text())
+	self.Severity=splitAndLast(doc.Find(".txt .field-field-severity").Last().Text())
+	self.Expoitable=splitAndLast(doc.Find(".txt .field-field-exploitable").Last().Text())
+
+	lastPos:=doc.Find(".txt .field-field-exploitable")
+	self.Description=lastPos.Next().Next().Next().Text()
+	return nil
+}
+
+
 
 func (bv *BindValunerability) String() string {
 	return fmt.Sprintf("[ID:%d] CVE=%s ShortDescription=%s Url=%s", bv.ID, bv.CVE, bv.ShortDescription, bv.Url)
@@ -58,9 +97,9 @@ func (bv *BindValunerability) String() string {
 
 type BindValunerabilityList struct {
 	sync.Mutex
-	Bvlist  []BindValunerability
+	Bvlist  []*BindValunerability
 	Status  status
-	Fetched int
+	Fetched int32
 	Url     string
 }
 
@@ -69,7 +108,7 @@ func NewBindValunerabilityList() *BindValunerabilityList {
 		Status:  QUERY_NOTSTART,
 		Fetched: 0,
 		Url:     ISC_VULNERABILITY_MATRIX,
-		Bvlist:  []BindValunerability{},
+		Bvlist:  []*BindValunerability{},
 	}
 }
 
@@ -108,35 +147,45 @@ func (self *BindValunerabilityList) Start() error {
 						}
 					}
 				})
-				self.Bvlist = append(self.Bvlist, *bv)
+				self.Bvlist = append(self.Bvlist, bv)
 			})
 		}
 	})
 
-	for _, item := range self.Bvlist {
-		println(item.String())
-	}
-	//log.Println(result)
 	self.Status = QUERY_READY
 	return nil
 }
-
-func (self *BindValunerabilityList) ShowFetchedNumber() int {
+func (self *BindValunerabilityList) ShowList() {
+	for _, item := range self.Bvlist {
+		println(item.String())
+	}
+}
+func (self *BindValunerabilityList) ShowFetchedNumber() int32 {
 	self.Lock()
 	defer self.Unlock()
 	return self.Fetched
 }
+//AddFetched 当有一个已经结束的时候，在Fetched上面加1
 func (self *BindValunerabilityList) AddFetched() {
-	self.Lock()
-	defer self.Unlock()
-	self.Fetched++
+	atomic.AddInt32(&self.Fetched,1)
 }
 
-func NewBindValunerabiltiy() *BindValunerability {
-	return &BindValunerability{
-		Status: QUERY_NOTSTART,
-		Detail: BindValunerabilityDetail{},
+func SaveListToFile(file string,bvl *BindValunerabilityList)error{
+	fp, err := os.Create(file)
+	if err != nil {
+		log.Panicf("Unable to create %v. Err: %v.", file, err)
+		return err
 	}
+	defer fp.Close()
+	j,err:=json.MarshalIndent(bvl,""," ")
+	if err!=nil{
+		return err
+	}
+	_, werr := fp.Write(j)
+	if werr != nil {
+		return werr
+	}
+	return nil
 }
 
 func (bv *BindValunerability) GetDetail() error {
@@ -151,8 +200,30 @@ func main() {
 		log.Panicf("Request Error : %s", err)
 		return
 	}
+	var wg sync.WaitGroup
+	requestChan:=make(chan struct{},MAXCONCURRENT)
 	if bvlist.Status == QUERY_READY {
 		log.Println("Will start concurrent query each item")
+		wg.Add(len(bvlist.Bvlist))
+		for _,b:=range(bvlist.Bvlist){
+			go func(b *BindValunerability){
+				defer wg.Done()
+				requestChan<-struct{}{}
+				err:=b.Detail.GetDetailInfo(b.CVE,b.Url)
+				if err!=nil{
+					b.Status=QUERY_ERROR
+				}else{
+					bvlist.AddFetched()
+				}
+				<-requestChan
+			}(b)
+		}
+		wg.Wait()
+		SaveListToFile("lastest.file",bvlist)
 	}
 	log.Println("Current Query Number is", bvlist.ShowFetchedNumber())
+
+
+
+
 }
